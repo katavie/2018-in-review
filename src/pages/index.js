271c4347c2efc965ef08a833c6fe6cd75452e879
Katherine Huang <hello@katherinehuang.co>
2018-12-30 21:52:46 -0500

    typography plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div>
+        <h1>2018 in Review</h1>
+        <p>2018 has been a year of challenges and growth, and I'm excited for 2019. Here's a recap of the past year, as best as I can remember.</p>
+        <h2>Winter</h2>
+        <p>In January, I participated in SheHacks, my first 36-hour hackathon! My team, composed of high school and college students from MA, NY, and IL, built S1REN, a dashboard that semi-automatically identifies aid requests among tweets during natural disasters. The event was huge, fun, and empowering, and to our surprise, we won Best Disaster Relief Hack!</p>
+    </div>
+)
